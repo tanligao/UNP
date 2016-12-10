b76767c8168e0d9421ef7af258682d547394e184
tanligao <tanligaogao@gmail.com>
2016-12-10 22:26:07 +0800

    update udp echo client.c

--- chapter8/echo_udp/client.c
+++ chapter8/echo_udp/client.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <strings.h>
 #include <string.h>
 
@@ -14,14 +15,48 @@
 #define oops(err_str,err_code) do { fprintf(stderr,"%s\n",err_str); \
 			exit(err_code); } while(0)
 
+// number to per
+char* Sock_ntop(const struct sockaddr *sa,socklen_t salen)
+{
+	char port_str[8];
+	static char str[128];		// include unix domain
+	
+	switch(sa->sa_family)
+	{
+		case AF_INET:
+		{
+			struct sockaddr_in *sa_in = (struct sockaddr_in*)&sa;
+			if( inet_ntop(AF_INET,&sa_in->sin_addr,str,sizeof(str)) == NULL )
+				return NULL;
+			if( ntohs(sa_in->sin_port) != 0 )
+			{
+				snprintf(port_str,sizeof(port_str),":%d",ntohs(sa_in->sin_port));
+				strcat(str,port_str);
+			}
+			return str;
+		}
+	} 
+}
+
 void dg_cli(FILE *fp,int sockfd,const struct sockaddr *ser_addr,socklen_t ser_len)
 {
 	int n;
-	char send_line[MAX_LINE],recv_line[MAX_LINE];
+	char send_line[MAX_LINE],recv_line[MAX_LINE+1];
+	socklen_t len;
+	struct sockaddr *pre_addr;
+	connect(sockfd,(struct sockaddr*)&ser_addr,ser_len);
+	pre_addr = malloc(ser_len);
 	while( fgets(send_line,MAX_LINE,fp) != NULL )
 	{
 		sendto(sockfd,send_line,strlen(send_line),0,ser_addr,ser_len);
-		n = recvfrom(sockfd,recv_line,MAX_LINE,0,NULL,NULL);
+
+		len = ser_len;
+		n = recvfrom(sockfd,recv_line,MAX_LINE,0,pre_addr,&len);
+		if( len != ser_len || memcmp(pre_addr,ser_addr,len))
+		{
+			printf("reply from %s (ignored)\n",Sock_ntop(pre_addr,len));
+			continue;
+		}
 		recv_line[n] = 0;
 		fputs(recv_line,stdout);
 	}
